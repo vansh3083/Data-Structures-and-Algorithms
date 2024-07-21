c39ec2c2f7044058226c2f7af3feaf0ebd59933d
Vansh Saini <cvsaini3083@gmail.com>
2024-07-21 16:39:19 +0530

    Uodate Search in String

--- SearchInString.java
+++ SearchInString.java
@@ -9,15 +9,15 @@ public class SearchInString {
     }
 
     static boolean search(String str, char target){
-       if(str.length() == 0){
-           return false;
-       }
+        if(str.length() == 0){
+            return false;
+        }
         for(int i =0; i<str.length(); i++){
-           if (target == str.charAt(i)){
-               return true;
-           }
+            if (target == str.charAt(i)){
+                return true;
+            }
 
-       }
-       return false;
+        }
+        return false;
     }
 }
