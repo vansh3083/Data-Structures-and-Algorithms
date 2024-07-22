4113ae4e922da7f021bc22a6f1ea9de3b26ca672
Vansh Saini <cvsaini3083@gmail.com>
2024-07-23 01:33:56 +0530

    Binary Search

---
+++ BinarySearch.java
@@ -0,0 +1,31 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        int target = 20;
+        System.out.println(binarySearch(arr, target));
+        target = 2;
+        System.out.println(binarySearch(arr, target));
+        target = 69;
+        System.out.println(binarySearch(arr, target));
+    }
+
+    static int binarySearch(int arr[], int target){
+        int start =0;
+        int end = arr.length-1;
+
+        while (start<=end){
+            int mid = (start + ((end - start)/2));
+            if(target < arr[mid]){
+                end = mid -1;
+            }
+            else if(target > arr[mid]){
+                start = mid +1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+
+    }
+}
