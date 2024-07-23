aebc8bfa02024e44f782adb0eca2baae3acab60c
Vansh Saini <cvsaini3083@gmail.com>
2024-07-23 21:25:14 +0530

    Floor of a Number

---
+++ FloorNumber.java
@@ -0,0 +1,26 @@
+public class FloorNumber {
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8,10,12,14,16,18,20};
+        int target = 5;
+        System.out.println(floorNumber(arr, target));
+    }
+    static int floorNumber(int[] arr, int target){
+        int start =0;
+        int end = arr.length-1;
+
+        while (start<=end){
+            int mid = (start + ((end - start)/2));
+            if(target < arr[mid]){
+                end = mid -1;
+            }
+            else if(target > arr[mid]){
+                start = mid +1;
+            }
+            else{
+                return arr[mid];
+            }
+        }
+        return arr[end];
+
+    }
+}
