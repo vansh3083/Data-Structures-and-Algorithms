1ff61615eb5179461d1004b8e99695178f745171
Vansh Saini <cvsaini3083@gmail.com>
2024-07-28 23:46:27 +0530

    Problem 34

--- FirstAndLastPosition34.java
+++ FirstAndLastPosition34.java
@@ -3,14 +3,11 @@ class Solution {
 
         int[] ans = {-1,-1};
 
-        int start = search(nums, target,true);
-        int end  =  search(nums, target, false);
-
-        ans[0]= start;
-        ans[1]=end;
-
+        ans[0] = search(nums, target,true);
+        if(ans[0]!=-1){
+            ans[1] = search(nums, target, false);
+        }
         return ans;
-
     }
 
     static int search(int[] nums, int target, boolean findStartIndex){
