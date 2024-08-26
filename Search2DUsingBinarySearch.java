06ff868f4775533d3850820eee9cd492a13e7ffd
Vansh Saini <cvsaini3083@gmail.com>
2024-08-26 10:54:55 +0530

    Searching in 2D Array using Binary Search

--- Search2DUsingBinarySearch.java
+++ Search2DUsingBinarySearch.java
@@ -4,13 +4,14 @@ public class Search2DUsingBinarySearch {
     public static void main(String[] args){
 
         int[][] arr = {
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
+                {10,20,30,40},
+                {15,25,35,45},
+                {28,29,37,49},
+                {33,34,38,50}
         };
 
-        System.out.println(Arrays.toString(search(arr,10)));
-        System.out.println(Arrays.toString(search(arr,9)));
+        System.out.println(Arrays.toString(search(arr,49)));
+        System.out.println(Arrays.toString(search(arr,20)));
         System.out.println(Arrays.toString(search(arr,1)));
     }
 
